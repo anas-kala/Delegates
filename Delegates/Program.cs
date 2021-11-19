c3b03ab080023d6f92858caf35cdf8f7990dabd9
Anas Al Kala <aal1580@students.fhv.at>
2021-11-19 10:40:29 +0100

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { Title = "video 1" };
+            var vedioEncoder = new VidoEncoder(); // publisher
+            var mailService = new MailService(); // subscriber
+            vedioEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            var messageService = new MessageService();
+            vedioEncoder.VideoEncoded += messageService.OnVideoEncoded;
+
+            vedioEncoder.Encode(video);
+        }
+    }
+
+    public class MailService
+    {
+        public void OnVideoEncoded(object source,VideoEventArgs e)
+        {
+            Console.WriteLine("Mail service: Sending an Email..." + e.Video.Title);
+        }
+    }
+
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e)
+        {
+            Console.WriteLine("MessageService: Sending a text message..."+e.Video.Title);
+        }
+    }
+
+}
---
+++ Delegates/VidoEncoder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Delegates
+{
+
+    public class VideoEventArgs: EventArgs
+    {
+        public Video Video { get; set; }
+    }
+
+    class VidoEncoder
+    {
+        //// define a delegate
+        //public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+
+        //// define an event based on that delegate
+        //public event VideoEncodedEventHandler VideoEncoded;
+
+        // define an event handler
+        public event EventHandler<VideoEventArgs> VideoEncoded;     //Without arguments it would be: public event EventHandler VideoEncoded;
+
+
+        // Raise the event
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            // check if there are subscribers
+            if (VideoEncoded != null)
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+        }
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            // Notifying subscribers
+            OnVideoEncoded(video);
+        }
+    }
+}
